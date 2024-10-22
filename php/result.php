c2de55a42e991675808ab3cbcccbf10ad6b312e2
clovislfbv <clovis.lefebvre@epita.fr>
2024-10-22 15:52:08 +0200

    add git pull icon in the website

--- php/result.php
+++ php/result.php
@@ -66,8 +66,9 @@
                 } else if (strlen($outputArray[$i]) > 0 && $outputArray[$i][0] == 'd') {
                     $git = 0;
                     $index = get_start_index($folders[$i-1], $outputArray[$i]);
+                    echo "<div class='container_icon_folder'>";
                     $newString = substr_replace($outputArray[$i], "<a id='" . $i-1 . "' class='folder' href='result.php'>", $index, 0);
-                    echo "<img class='img' src='../assets/folder.gif' alt='folder'><h3>" . $newString . "<br></h3></a>";
+                    echo "<img class='img' src='../assets/folder.gif' alt='folder'><h3>" . $newString . "<br></h3></a></div>";
                     
                     $stream = ssh2_exec($connection, 'cd ' . $_SESSION['current'] . $folders[$i-1] . '&& ls -a');
                     stream_set_blocking($stream, true);
@@ -83,20 +84,22 @@
                         }
                     }
 
-                    if ($git == 1)
+                    if ($git == 1 && $folders[$i-1] != ".")
                     {
-                            print_r("git repo found");
+                        echo "<img class='icon git_pull' id='pull_" . $i-1 . "' src='../assets/git-pull-request-icon.png' alt='git-pull-request-icon'>";
+                        //print_r("git repo found");
                     }
                     
                 } else if (count($outputArray) - 1 != $i){
                     $index = get_start_index($folders[$i-1], $outputArray[$i]);
+                    echo "<div class='container_icon_folder'>";
                     $newString = substr_replace($outputArray[$i], "<a id='" . $i-1 . "' class='file' href='result.php'>", $index, 0);
                     echo "<img class='img' src='../assets/text.gif' alt='file'><h3>" . $newString . "<br></h3></a>";
-                    echo "<button type='button' id='preview_" . $i-1 . "' class='btn btn-primary preview'><svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-eye' viewBox='0 0 16 16'><path d='M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8M1.173 8a13 13 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5s3.879 1.168 5.168 2.457A13 13 0 0 1 14.828 8q-.086.13-.195.288c-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5s-3.879-1.168-5.168-2.457A13 13 0 0 1 1.172 8z'></path><path d='M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5M4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0'></path></svg></button>";
+                    echo "<div class='container_button'><button type='button' id='preview_" . $i-1 . "' class='btn btn-primary preview'><svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-eye' viewBox='0 0 16 16'><path d='M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8M1.173 8a13 13 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5s3.879 1.168 5.168 2.457A13 13 0 0 1 14.828 8q-.086.13-.195.288c-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5s-3.879-1.168-5.168-2.457A13 13 0 0 1 1.172 8z'></path><path d='M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5M4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0'></path></svg></button></div></div>";
                 }
 
                 if ($i > 2 && count($outputArray) - 1 != $i){
-                    echo "<img class='poubelle' id='del_" . $i-1 . "' src='../assets/bin.png' alt='bin-logo'>";
+                    echo "<img class='icon poubelle' id='del_" . $i-1 . "' src='../assets/bin.png' alt='bin-logo'></div>";
                 }
                 echo "</div>";
             }
